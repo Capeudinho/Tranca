d005b4dbeb73b344a8d5352032891902d7bb8978
unknown <archmagepyromancer@gmail.com>
2020-04-24 20:15:18 -0300

    test commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import "./global.css";
+import "./App.css"
+
+function App() {
+  return (
+    <div id = "app">
+      <header className = "header">
+        <div className = "profilePicture"></div>
+        <div className = "profileName">Rafael Carvalho</div>
+        <div className = "search">Pesquisar</div>
+        <div className = "menu">Menu</div>
+      </header>
+      <div className = "main">
+        <div className = "section sectionLeft">
+          <div className = "area areaRecents">
+            <div className = "list listRecents">
+              <button className = "fileButton">
+                Recentes
+              </button>
+            </div>
+          </div>
+          <div className = "area areaLocks">
+            <div className = "list listLocks">
+              <button  className = "fileButton">
+                Trancas
+              </button>
+              <div className = "explorer">
+                <div className = "item itemLeft itemComponent">
+                  <button className = "arrow">></button>
+                  <button className = "name">Bloco E</button>
+                </div>
+                <div className = "item itemLeft itemComponent">
+                  <button className = "arrow">></button>
+                  <button className = "name">Bloco D</button>
+                </div>
+                <div className = "item itemLeft itemComponent">
+                  <button className = "arrow">></button>
+                  <button className = "name">Bloco C</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className = "area areaUsers">
+            <div className = "list listUsers">
+              <button className = "fileButton">
+                Usuários
+              </button>
+              <div className = "explorer">
+                <div className = "item itemLeft itemUser">
+                  <button className = "name">Maria Clara</button>
+                </div>
+                <div className = "item itemLeft itemUser">
+                  <button className = "name">João Paulo</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className = "section sectionCenter">
+          <button className = "item itemCenter">Lab 12</button>
+          <button className = "item itemCenter">Lab 13</button>
+        </div>
+
+        <div className = "section sectionRight">
+            <p className = "name">Lab 12</p>
+            <p className = "type">Sala</p>
+          <div>
+            <button className = "buttonAction buttonEdit">Editar</button>
+            <button className = "buttonAction buttonMove">Mover</button>
+            <button className = "buttonAction buttonExclude">Excluir</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
